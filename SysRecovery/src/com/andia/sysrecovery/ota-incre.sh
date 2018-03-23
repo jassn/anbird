5ea201088c2edeb5828f8efd1c0015267f86b052
jason <jsnsgt@gmail.com>
2018-03-23 09:30:31 +0800

    ota-incre

--- SysRecovery/src/com/andia/sysrecovery/ota-incre.sh
+++ SysRecovery/src/com/andia/sysrecovery/ota-incre.sh
@@ -64,7 +64,25 @@ MM_DIR=""
 LOG=""
 
 
+###########################################
 
+export COLOR_RESET='\e[0m' # No Color
+export COLOR_WHITE='\e[1;37m'
+export COLOR_BLACK='\e[0;30m'
+export COLOR_BLUE='\e[0;34m'
+export COLOR_LIGHT_BLUE='\e[1;34m'
+export COLOR_GREEN='\e[0;32m'
+export COLOR_LIGHT_GREEN='\e[1;32m'
+export COLOR_CYAN='\e[0;36m'
+export COLOR_LIGHT_CYAN='\e[1;36m'
+export COLOR_RED='\e[0;31m'
+export COLOR_LIGHT_RED='\e[1;31m'
+export COLOR_PURPLE='\e[0;35m'
+export COLOR_LIGHT_PURPLE='\e[1;35m'
+export COLOR_BROWN='\e[0;33m'
+export COLOR_YELLOW='\e[1;33m'
+export COLOR_GRAY='\e[0;30m'
+export COLOR_LIGHT_GRAY='\e[0;37m'
 
 check_arg
 # build $ARG $MM_DIR 
@@ -78,26 +96,34 @@ PRODUCT=sabresd_6dq
     
 ## PREV_NUM=20180319
 if [ "x$PREV_NUM" = "x" ]; then
-    PREV_NUM=20180319
+    PREV_NUM=20180322
 fi
 
-CURR_NUM=20180321
+CURR_NUM=20180323
 
 PREV_PATH=$BASE_PATH/dist-$PREV_NUM
 CURR_PATH=$BASE_PATH/dist-$CURR_NUM
 
 PREV_TARGET=$PREV_PATH/$PRODUCT-target_files-$PREV_NUM.zip
 CURR_TARGET=$CURR_PATH/$PRODUCT-target_files-$CURR_NUM.zip
+FULL_OTA=$BASE_PATH/$PRODUCT-ota-$CURR_NUM-jj.zip
 INCRE_OTA=$BASE_PATH/$PRODUCT-incre-$PREV_NUM-to-$CURR_NUM.zip
 
-echo "Old ...... $PREV_TARGET"
-echo "New ...... $CURR_TARGET"
-echo "Generating incremental OTA ...... $INCRE_OTA"
+
+echo -e "Old ...... $PREV_TARGET"
+echo -e "New ...... $CURR_TARGET"
+echo -e "${COLOR_GREEN}
+echo -e "Generating incremental OTA ...... ${INCRE_OTA}${COLOR_RESET}"
 
 build/tools/releasetools/ota_from_target_files.py \
 -i $PREV_TARGET $CURR_TARGET $INCRE_OTA
 
-
+##
+##
+echo -e "${COLOR_GREEN}"
+echo -e "Generating Full OTA ...... ${FULL_OTA}${COLOR_RESET}"
+build/tools/releasetools/ota_from_target_files.py \
+$CURR_TARGET $FULL_OTA
 
 
 
