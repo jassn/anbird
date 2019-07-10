a698571ebc638482fe12a8518dbca8abe95b96e0
jason <jsnsgt@gmail.com>
2019-07-10 16:35:23 +0800

    [GTK] Add a button - Close window.

--- gtk/gtktest.c
+++ gtk/gtktest.c
@@ -25,8 +25,12 @@ gcc -o gtktest gtktest.c `pkg-config --cflags --libs gtk+-2.0`
 int main (int argc, char *argv[])
 {
 	gtk_init(&argc, &argv);
+
 	GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_widget_show(win);
+	GtkWidget *btn = gtk_button_new_with_label("Close window");
+	gtk_container_add(GTK_CONTAINER (win), btn);
+
+	gtk_widget_show_all(win);
 	gtk_main();
 
 	return 0;
